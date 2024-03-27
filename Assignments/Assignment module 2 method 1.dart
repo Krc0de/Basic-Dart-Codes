113b775b814bc471962d877a7925494d22bbf0d4
Krc0de <155648008+Krc0de@users.noreply.github.com>
2024-03-27 22:12:51 +0600

    New Assignment

---
+++ Assignments/Assignment module 2 method 1.dart
@@ -0,0 +1,30 @@
+
+void main () {
+  List<int> numbers = [85, 92, 78, 65, 88, 72];
+  double sum = sumUsingLoop(numbers);
+  double average = sum / numbers.length;
+
+  print("Student's average grade:"
+      "$average");
+
+  int mark  = average.toInt();
+  print("Rounded average : $mark" );
+
+  if (mark>=70) {print('Passed');}
+  else {print('Failed');}
+
+
+}
+double sumUsingLoop(List<int>numbers){
+  double sum=0;
+
+ for (int number in numbers) {
+    print(number);
+     sum += number;
+  }
+  return sum;
+
+//(mistake) int RA = average.toInt();
+ print("Rounded average : RA");
+
+}
